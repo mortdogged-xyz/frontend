771583b5909dba09a825b7a604e1955e2690f9e1
Max Gonzih <gonzih@gmail.com>
2022-07-16 15:34:30 -0700

    Huh?

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -45,7 +45,7 @@ var uiConfig = {
         // Leave the lines as is for the providers you want to offer your users.
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         // firebase.auth.GithubAuthProvider.PROVIDER_ID,
         // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
@@ -78,6 +78,7 @@ export const AuthUI = (props: {
     useEffect(() => {
         firebase.auth().onAuthStateChanged((user) => {
             if (user) {
+                console.log(user);
                 onLoginChange(user.uid);
             } else {
                 onLoginChange(null);
