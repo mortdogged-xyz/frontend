28f7ec9303d55007f43da5894724e4f88c0b475a
Max Gonzih <gonzih@gmail.com>
2022-07-17 16:12:19 -0700

    Add sentry

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://7372455626154f96a2a89515d3b6da61@o1323051.ingest.sentry.io/6580264",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
 function render_react() {
     const root = ReactDOM.createRoot(
         document.getElementById('root') as HTMLElement
