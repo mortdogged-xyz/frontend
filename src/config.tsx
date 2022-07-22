f57f080957ec84c8bc9a09331c165db9dd306870
Max Gonzih <gonzih@gmail.com>
2022-07-21 21:29:48 -0700

    Try this proxy thing

--- src/config.tsx
+++ src/config.tsx
@@ -1,8 +1,8 @@
 import { TFTSet, TFTVersion } from './version';
 
-export let exportURL = `https://us-central1-tft-meta-73571.cloudfunctions.net/exportResponsesV2?TFTSet=${TFTSet}&TFTVersion=${TFTVersion}&token=`;
-export let loginURL = "https://us-central1-tft-meta-73571.cloudfunctions.net/loginV2";
-export let submitURL = "https://us-central1-tft-meta-73571.cloudfunctions.net/submitV2";
+export let exportURL = `/exportResponsesV2?TFTSet=${TFTSet}&TFTVersion=${TFTVersion}&token=`;
+export let loginURL = "/loginV2";
+export let submitURL = "/submitV2";
 
 if (process.env.NODE_ENV !== 'production') {
     exportURL = `http://localhost:5001/tft-meta-73571/us-central1/exportResponsesV2?TFTSet=${TFTSet}&TFTVersion=${TFTVersion}&token=`;
