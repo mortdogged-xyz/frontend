16a396a53b4e3bff4cb3b6ba1fc3d2aaab43d763
Max Gonzih <gonzih@gmail.com>
2022-08-07 18:42:46 -0700

    Ugh

--- src/set_data.ts
+++ src/set_data.ts
@@ -24,22 +24,47 @@ interface RawData {
   };
 }
 
+const blacklist = ['Jade Statue', 'The Golden Egg', "Zoe's Daisy"];
+const whitelist = [
+  'Exiles',
+  'True-Twos',
+  'Thrill',
+  'Ascension',
+  'Lotus',
+  'Grab',
+  'Recruit',
+  'Level',
+  'Ticket',
+];
+
+function offBlacklist(item: DataItem) {
+  return !blacklist.includes(item.name);
+}
+
 const data = rawData as unknown as RawData;
 
+export const traits = data.sets[TFTSetNumber].traits
+  .filter(offBlacklist)
+  .map((item: DataItem) => item.name);
+
 export const items = data.items
+  .filter(offBlacklist)
   .filter((item: DataItem) => !item.icon.includes('Augments/Hexcore'))
   .map((item: DataItem) => item.name || '');
 export const augs = data.items
+  .filter(offBlacklist)
+  .filter(
+    (item: DataItem) =>
+      item.icon.includes(CurrentSet) ||
+      whitelist.some((wl) => item.icon.includes(wl)) ||
+      traits.some((trait) => item.icon.includes(trait)),
+  )
   .filter((item: DataItem) => item.icon.includes('Augments/Hexcore'))
   .map((item: DataItem) => item.name || '');
 
-export const traits = data.sets[TFTSetNumber].traits.map(
-  (item: DataItem) => item.name,
-);
-
-export const champs = data.sets[TFTSetNumber].champions.map(
-  (item: DataItem) => item.name,
-);
+export const champs = data.sets[TFTSetNumber].champions
+  .filter(offBlacklist)
+  .map((item: DataItem) => item.name);
 
 export const champ_cost: Record<string, number> = data.sets[
   TFTSetNumber
