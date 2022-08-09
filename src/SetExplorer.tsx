6309292b34adba75c2c90236052805f20de0aa72
Max Gonzih <gonzih@gmail.com>
2022-08-09 13:04:21 -0700

    Nicer header

--- src/SetExplorer.tsx
+++ src/SetExplorer.tsx
@@ -2,11 +2,14 @@ import React, {useState} from 'react';
 
 import RawData from './data/en_us.json';
 
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import Toolbar from '@mui/material/Toolbar';
 
 import {TFTSetNumber} from './version';
 import {CurrentSet, icon2Src} from './set_data';
@@ -70,25 +73,42 @@ export const SetExplorer = () => {
 
   return (
     <Box>
-      <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
-        <Tabs value={value} onChange={handleChange} centered>
-          {allTabs.map((tab) => (
-            <Tab label={tab} value={tab} key={tab} />
-          ))}
-        </Tabs>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar
+            disableGutters
+            sx={{
+              borderBottom: 1,
+              borderColor: 'divider',
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: '20px',
+            }}
+          >
+            <Box component="span" sx={{maxWidth: 580}}>
+              <Tabs value={value} onChange={handleChange} centered>
+                {allTabs.map((tab) => (
+                  <Tab label={tab} value={tab} key={tab} />
+                ))}
+              </Tabs>
+            </Box>
+
+            <Box component="span" sx={{maxWidth: 280}}>
+              <Search placeholder={'Search...'} onChange={setFilter} />
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      <Box>
+        {items.map((item) => (
+          <Item
+            item={item}
+            tab={value}
+            key={`${item.id}${item.name}${item.apiName}`}
+          />
+        ))}
       </Box>
-
-      <Paper component="div" sx={{margin: '50px 60px'}}>
-        <Search placeholder={'Search...'} onChange={setFilter} />
-      </Paper>
-
-      {items.map((item) => (
-        <Item
-          item={item}
-          tab={value}
-          key={`${item.id}${item.name}${item.apiName}`}
-        />
-      ))}
     </Box>
   );
 };
