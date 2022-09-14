4d6c15d45033922d6ec9ecf6258ea3ebf775aa61
Max Gonzih <gonzih@gmail.com>
2022-09-14 13:15:38 -0700

    Patch update

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 export const TFTSet = '7.5';
 export const TFTSetNumber = 7;
-export const TFTVersion = '12.17';
+export const TFTVersion = '12.17b';
 export const StorageKey = `${TFTSet}-${TFTVersion}`;
