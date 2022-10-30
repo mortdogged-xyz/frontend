8b010e933a2ae4539b46727e7a5af63ca5390823
Max Gonzih <gonzih@gmail.com>
2022-10-30 14:00:09 -0700

    12.21

--- src/version.ts
+++ src/version.ts
@@ -1,4 +1,4 @@
 export const TFTSet = '7.5';
 export const TFTSetNumber = 7;
-export const TFTVersion = '12.19';
+export const TFTVersion = '12.21';
 export const StorageKey = `${TFTSet}-${TFTVersion}`;
